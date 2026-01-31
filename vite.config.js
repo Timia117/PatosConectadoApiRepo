a35b2b78a3914644b5ba807583a0e94269d26bf0
a <a>
2026-01-31 15:48:26 +0100

    prueba1

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-
-// https://vite.dev/config/
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { playwright } from '@vitest/browser-playwright';
 
 const dirname = typeof __dirname !== 'undefined'
   ? __dirname
@@ -14,16 +10,13 @@ const dirname = typeof __dirname !== 'undefined'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  test: {
-    browser: {
-      enabled: true,
-      headless: true,
-      provider: playwright({}),
-      instances: [
-        {
-          browser: 'chromium'
-        }
-      ]
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(dirname, 'src')
     }
-  }
+  },
+  base: './' 
 });
